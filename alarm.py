a272fcc857b58a239094aef3b4c214217c2ed2f3
Ananya Maity <115604028+ananyamaity00@users.noreply.github.com>
2024-07-01 20:37:41 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,17 @@
+import datetime
+
+from playsound import playsound
+
+alarmHour = int(input("Enter Hour: "))
+alarmMinutes = int(input("Enter Minutes: "))
+alarmAm = input("am / pm: ")
+
+if alarmAm == "pm":
+    alarmHour += 12
+
+while True:
+    now = datetime.datetime.now()
+    if alarmHour == now.hour and alarmMinutes == now.minute:
+        print("Wake Up!")
+        playsound("alarm.mp3")
+        break
